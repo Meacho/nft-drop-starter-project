fd19887351796a1c6e94d546a2733c2dc5bc9cb8
Meacho <Meacho@users.noreply.github.com>
2022-02-26 12:29:13 +1100

    Added files

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,99 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./Content/Footer";
+import CandyMachine from "./CandyMachine";
+import marcafiLogo from "./assets/logo-large.png";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  //state
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  /* Actions
+   *  Declare function
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet */
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      //check if wallet has injected solana in DOM window
+      const { solana } = window;
+      //check if the wallet is the Phantom wallet extension
+      if (solana && solana.isPhantom) {
+        console.log("Phantom wallet found!");
+
+        /*
+         * The solana object gives us a function that will allow us to connect
+         * directly with the user's wallet!
+         */
+
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          "Connected with Public Key:",
+          response.publicKey.toString()
+        );
+        //Set users publickey to state for later use
+
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert("Solana object not found. Get phantom wallet");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //Define methods so code doesn't break
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log(
+        "Connected from connect button logic with Public key:",
+        response.publicKey.toString()
+      );
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  // render not connected container
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  //useEffect called when component loaded and [] is empty so basically when page loads
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+          <img alt="Marcafi Logo" className="marcafi-logo" src={marcafiLogo} />
+          <p className="header">Membership</p>
+          <p className="sub-text">
+            To get your complimentary Marcafi membership please connect your
+            wallet and follows the on screen instructions.
+          </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
+
+        {/* Check for walletAddress and then pass in walletAddress */}
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
       </div>
+      {Footer()}
     </div>
   );
 };
